07150568140cc6a1cee44d8478c604aa46b1c122
Atchiam <braian.ferreyra@hotmail.com>
2022-06-13 21:17:54 -0300

    html

--- Django/MTV_Braian/MTV_Braian/urls.py
+++ Django/MTV_Braian/MTV_Braian/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from MTV_BraianApp.views import *
+from MTV_BraianApp.views import inicio,familia
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", inicio),
+    path("fa", familia),
 ]
--- Django/MTV_Braian/MTV_BraianApp/views.py
+++ Django/MTV_Braian/MTV_BraianApp/views.py
@@ -1,7 +1,16 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+
+from MTV_BraianApp.models import Familia
 
 # Create your views here.
 
 def inicio(request):
     return render(request, "index.html",{}) #return render(request, "nombre del HTML",{Argumentos o contexto "lo que se modifica o entra en el HTML"})
 
+def familia(request):
+    familias = Familia.objects.all()
+    
+    ctx = {"familias": familias}
+    
+    return render (request, "famila.html", ctx)
